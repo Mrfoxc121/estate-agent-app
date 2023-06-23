059379971ec0d0da8dc0e82fb9bb524d0b1468b4
Mrfoxc121 <cfox0115@gmail.com>
2023-06-23 09:30:18 +0100

    complete OAuth functionality

--- src/components/OAuth.jsx
+++ src/components/OAuth.jsx
@@ -1,16 +1,52 @@
-import React from 'react'
+import React from "react";
 import { FcGoogle } from "react-icons/fc";
+import { GoogleAuthProvider, signInWithPopup, getAuth } from "firebase/auth";
+import { toast } from "react-toastify";
+import { db } from "../firebase";
+import { doc, getDoc, serverTimestamp, setDoc } from "firebase/firestore";
+import { useNavigate } from "react-router";
+
+
 
 function OAuth() {
+  const navigate = useNavigate()
+  async function onGoogleClick(){
+
+    try {
+      const auth = getAuth()
+      const provider = new GoogleAuthProvider()
+      const result = await signInWithPopup(auth, provider)
+      const user = result.user
+      console.log(user)
+
+      // check for user
+
+      const docRef = doc(db, "users", user.uid);
+      const docSnap = await getDoc(docRef);
+
+      if(!docSnap.exists()){
+        await setDoc(docRef, {
+          name: user.displayName,
+          email: user.email,
+          timestamp: serverTimestamp(),
+        })
+      }
+      navigate("/");
+
+    } catch (error) {
+      toast.error("Could not authorise with google")
+      console.log(error)
+    }
+
+  }
   return (
-    <button
-    type="button"
-    className="flex items-center justify-center w-full bg-red-700 text-white px-7 py-3 uppercase text-sm font-medium hover:bg-red-800 active:bg-red-900 shadow-md hover:shadow-lg active:shadow-lg transition duration-150 ease-in-out rounded"
-  >
-    <FcGoogle className="text-2xl  bg-white rounded-full mr-2" />
-    Continue with Google
-  </button>
-  )
+    <button type="button" onClick={onGoogleClick}
+      className="flex items-center justify-center w-full bg-red-700 text-white px-7 py-3 uppercase text-sm font-medium hover:bg-red-800 active:bg-red-900 shadow-md hover:shadow-lg active:shadow-lg transition duration-150 ease-in-out rounded"
+    >
+      <FcGoogle className="text-2xl  bg-white rounded-full mr-2" />
+      Continue with Google
+    </button>
+  );
 }
 
-export default OAuth
+export default OAuth;

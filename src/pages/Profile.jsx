89bbb4065d4fafee1a8124b44ca4d891cce54f51
Mrfoxc121 <cfox0115@gmail.com>
2023-06-30 09:12:43 +0100

    create listing page and use swiper to add image slider

--- src/pages/Profile.jsx
+++ src/pages/Profile.jsx
@@ -4,7 +4,6 @@ import { useNavigate } from "react-router";
 import { FcHome } from "react-icons/fc";
 import { toast } from "react-toastify";
 import {
-  Query,
   collection,
   deleteDoc,
   doc,
